322f5d568c6af7c24dee253cd263187d39691d8e
Gustavo Cohgus <162891550+cohgus@users.noreply.github.com>
2024-12-07 23:37:47 -0300

    Add files via upload

---
+++ template-materialui/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import login from "./pages/login";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <login />
+    </div>
+  );
+}
+
+export default App;
